2e856d11bda8c261c655eace5a14f9285a8903c3
sarah alsinan <salsinan@protonmail.com>
2022-08-30 16:47:38 -0700

    enable first three states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useReducer, useState, useRef } from "react";
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs' ;
 import './App.css';
 
 const stateMachine = {
-  initial: { on: { next: 'loadingModel' } },
-  loadingModel: { on: { next: 'awaitingUpload' } },
-  awaitingUpload: { on: { next: 'ready' } },
-  ready: { on: { next: 'classifying' } },
-  classifying: { on: { next: 'complete' } },
-  complete: { on: { next: 'awaitingUpload' } }
+  initial: 'initial',
+  states: {
+    initial: { on: { next: 'loadingModel' } },
+    loadingModel: { on: { next: 'awaitingUpload' } },
+    awaitingUpload: { on: { next: 'ready' } },
+    ready: { on: { next: 'classifying' }, showImage: true },
+    classifying: { on: { next: 'complete' } },
+    complete: { on: { next: 'awaitingUpload' }, showImage: true }
+  }
 }
 
+const reducer = (currentState, event) =>
+  stateMachine.states[currentState].on[event] || stateMachine.initial;
+
 function App() {
+  tf.setBackend("cpu");
+  const [state, dispatch] = useReducer(reducer, stateMachine.initial);
+  let [imageUrl, setImageUrl] = useState(null);
+  let [model, setModel] = useState(null);
+  const inputRef = useRef();
+  const imageRef = useRef();
+
+  const next = () => dispatch('next')
+
+  const loadModel = async () => {
+    next();
+    const mobilenetModel = await mobilenet.load();
+    setModel = mobilenetModel;
+    next();
+  }
+
+  const handleUpload = e => {
+    const { files } = e.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageUrl(url);
+      next();
+    }
+  }
+
+  const buttonProps = {
+    initial: { text: 'Load Model', action: loadModel },
+    loadingModel: { text: 'Loading Model...', action: () => {} },
+    awaitingUpload: { text: 'Upload Photo', action: () => inputRef.current.click() },
+    ready: { text: 'Identify', action: () => {} },
+    classifying: { text: 'Identifying', action: () => {} },
+    complete: { text: 'Reset', action: () => {} }
+  }
+
+  const { showImage = false} = stateMachine.states[state];
+
   return (
     <div>
+      {showImage && <img alt="upload preview" src={imageUrl} ref={imageRef}/>}
+      <input type="file" accept="image/*" capture="camera" ref={inputRef} onChange={handleUpload}/>
+      <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
     </div>
   );
 }
